ee534902f87d6a53df4a86a8784773b3bc46437e
ankynator <ankynator@gmail.com>
2020-12-26 23:20:17 -0500

    Obteniendo solo un task de firebase y pintandolo en nuestro formulario

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const db = firebase.firestore()
 const taskForm = document.getElementById("task-form")
 const tasksContainer = document.getElementById("tasks-container")
 
+let editStatus = false
+
 const saveTask = (title, description) => 
   db.collection('tasks').doc().set({
     title: title,
@@ -11,17 +13,19 @@ const saveTask = (title, description) =>
 
 const getTasks = () => db.collection('tasks').get()
 
+const getTask = (id) => db.collection('tasks').doc(id).get()
+
 const onGetTasks = (callback) => db.collection('tasks').onSnapshot(callback)
+
+const deleteTask = id => db.collection('tasks').doc(id).delete()
 
 window.addEventListener('DOMContentLoaded', async () => {
   onGetTasks((querySnapshot) => {
     tasksContainer.innerHTML = ''
 
     querySnapshot.forEach(doc => {
-
-      console.log(doc.data().title)
       const task = doc.data()
-  
+      task.id = doc.id
       tasksContainer.innerHTML += `
       <div class="card card-body mt-2 border-primary">
         <h5>
@@ -31,12 +35,32 @@ window.addEventListener('DOMContentLoaded', async () => {
           ${task.description}
         </p>
         <div>
-          <button class="btn btn-primary">Delete</button>
-          <button class="btn btn-secondary">Edit</button>
+          <button class="btn btn-primary btn-delete" data-id="${task.id}">Delete</button>
+          <button class="btn btn-secondary btn-edit" data-id="${task.id}">Edit</button>
         </div>
       </div>`
-  
     })
+
+    const btnsDelete = document.querySelectorAll('.btn-delete')
+
+    btnsDelete.forEach((btn) => {
+      btn.addEventListener('click', async (e) => {
+        await deleteTask(e.target.dataset.id)
+      })
+    })
+
+    const btnsEdit = document.querySelectorAll('.btn-edit')
+
+    btnsEdit.forEach((btn) => {
+      btn.addEventListener('click', async (e) => {
+        const doc = await getTask(e.target.dataset.id)
+        const task = doc.data()
+        
+        taskForm['task-title'].value = task.title
+        taskForm['task-description'].value = task.description
+      })
+    })
+
   })
 })
 
